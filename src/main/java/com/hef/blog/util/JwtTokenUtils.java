d7b02f1ab1ccc2b2613588203f7198d99a1263f0
Hefeng <chhefeng@gmail.com>
2020-07-03 14:31:42 +0200

    JwtTokenUtils is finished

--- src/main/java/com/hef/blog/util/JwtTokenUtils.java
+++ src/main/java/com/hef/blog/util/JwtTokenUtils.java
@@ -8,43 +8,37 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.DynamicTest;
 
 import javax.crypto.SecretKey;
+import java.security.Key;
 
 @Log4j2
 public class JwtTokenUtils {
 
-    private static SecretKey KEY= Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final String STRINGKEY = "iPrBkYvpb3ddf2FtGEZ-GzMTQKhrUzJzyNM6RKpZHMf";
 
-    public String createToken(String username) {
+
+    public static String generateToken(String username) {
+        Key key = Keys.hmacShaKeyFor(STRINGKEY.getBytes());
         String token = Jwts.builder()
                 .setSubject(username)
-                .signWith(KEY)
+                .signWith(key)
                 .compact();
         return token;
     }
 
 
     public static String getUsernameByToken(String token) {
-        return getTokenBody(token).getSubject();
-    }
-
-
-    private static Claims getTokenBody(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(KEY)
+        String username =Jwts.parserBuilder()
+                .setSigningKey(STRINGKEY)
                 .build()
                 .parseClaimsJws(token)
-                .getBody();
+                .getBody().getSubject();
+        return username;
     }
 
-    /**
-     * 验证JWT
-     */
-    public Boolean validateToken(String token, String username) {
 
+    public static Boolean validateToken(String token, String username) {
         String username1 = getUsernameByToken( token );
-
         return username.equals(username1);
-
     }
 
 }
