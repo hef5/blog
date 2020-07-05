781130c7f195e0eedb92caee570c4489c7f4cfbc
Hefeng <chhefeng@gmail.com>
2020-07-05 18:02:55 +0200

    request work with token correctly

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,19 +1,19 @@
-const URL_BLOG = "/admin/blogs";
-const URL_TYPE = "/admin/types";
-const URL_TAG = "/admin/tags";
-const URL_LOGOUT = "/admin/layout";
-const GET = "GET";
-const POST = "POST";
+const URL_BLOG        = "/admin/blogs";
+const URL_TYPE        = "/admin/types";
+const URL_TAG         = "/admin/tags";
+const URL_LOGOUT      = "/admin/layout";
+const URL_BLOG_INPUT  = "/admin/blogs/input";
+const URL_BLOG_SEARCH = "/admin/blogs/search";
 
-function StateObj(url, type) {
+
+function StateGet(url) {
     this.url = url;
-    this.type = type;
 }
 
-function ajax(url, type) {
+function ajaxGet(url) {
     $.ajax({
         url: url,
-        type: type,
+        type: "GET",
         beforeSend: function (xhr) {
             xhr.setRequestHeader('Authorization', sessionStorage.getItem('token'));
         },
@@ -25,68 +25,113 @@ function ajax(url, type) {
 
 $("#blogs").on("click",
     function (e) {
-        ajax(URL_BLOG, GET);
-        history.pushState(new StateObj(URL_BLOG, GET), null, "/admin/blogs.html");
+        ajaxGet(URL_BLOG);
+        history.pushState(new StateGet(URL_BLOG), null, "/admin/blogs.html");
     }
 ),
 
 $("#tags").on("click",
     function (e) {
-        ajax(URL_TAG, GET);
-        history.pushState(new StateObj(URL_TAG, GET), null, "/admin/tags.html");
+        ajaxGet(URL_TAG);
+        history.pushState(new StateGet(URL_TAG), null, "/admin/tags.html");
     }
 ),
 
 $("#types").on("click",
     function (e) {
-        ajax(URL_TYPE, GET)
-        history.pushState(new StateObj(URL_TYPE, GET), null, "/admin/types.html");
+        ajaxGet(URL_TYPE)
+        history.pushState(new StateGet(URL_TYPE), null, "/admin/types.html");
     }
 ),
 
 $("#layout").on("click",
     function (e, token) {
         e.preventDefault();
-        ajax(URL_LOGOUT, GET)
-        history.pushState(new StateObj(URL_LOGOUT, GET), null, "/admin/blogs.html");
+        ajaxGet(URL_LOGOUT)
+        history.pushState(new StateGet(URL_LOGOUT), null, "/admin/blogs.html");
     }
 ),
 
 $("#userName").text(sessionStorage.getItem('userName')),
 $("#userAvatar").attr("src", sessionStorage.getItem('userAvatar'))
 
-window.onpopstate = function (e) {
-    let stateObj = history.state;
-    ajax(stateObj.url, stateObj.type);
-}
 
-/** blog list Module */
-
+/** ------------------- blog list Module ------------------------ */
 function confirmDelete(blogId) {
     let url = "/admin/blogs/" + blogId + "/delete";
+    // noinspection JSUnresolvedFunction
     $('.ui.modal')
         .modal('show');
     $("#confirm-btn").on("click",
         function (e) {
             e.preventDefault();
-            ajax(url, GET);
+            ajaxGet(url);
         }
     )
 }
 
-$("#add-blog").on("click",
+$(document).on("click", "#add-blog",
     function (e) {
         e.preventDefault();
-        $.ajax({
-            url: "/admin/blogs/input",
-            type: "GET",
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', sessionStorage.getItem('token'));
-            },
-            success: function (data) {
-                $("body").html(data)
-            }
-        })
-        history.pushState(null, null, "/admin/blogs/input");
+        ajaxGet(URL_BLOG_INPUT);
+        history.pushState(new StateGet(URL_BLOG_INPUT), null, URL_BLOG_INPUT);
     }
 )
+
+$(document).on("click", "#search-btn",
+    function (e) {
+        e.preventDefault();
+        let data = {
+            title: $("[name='title']").val(),
+            typeId: $("[name='typeId']").val(),
+            recommend: $("[name='recommend']").prop('checked'),
+            page: "0"
+        }
+        ajaxPost(URL_BLOG_SEARCH, data);
+        history.pushState(new StatePost(URL_BLOG_SEARCH, data), null, URL_BLOG_SEARCH);
+    }
+)
+
+function editBlog (blogId) {
+    let url = "/admin/blogs/" + blogId + "/update";
+    ajaxGet(url)
+    history.pushState(new StateGet(url), null, url);
+}
+
+function otherPage (pageNumber) {
+    let data = {
+        title: $("[name='title']").val(),
+        typeId: $("[name='typeId']").val(),
+        recommend: $("[name='recommend']").prop('checked'),
+        page: pageNumber
+    };
+    ajaxPost(URL_BLOG_SEARCH, data);
+    history.pushState(new StatePost(URL_BLOG_SEARCH, data), null, URL_BLOG_SEARCH);
+}
+
+function ajaxPost (url, postData) {
+    $.ajax({
+        url: url,
+        type: "POST",
+        data: postData,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', sessionStorage.getItem('token'));
+        },
+        success: function (data) {
+            $("#table-container").html(data)
+        }
+    })
+}
+
+function StatePost(url, data) {
+    this.url = url;
+    this.data = data;
+}
+
+window.onpopstate = function (e) {
+    if (e.state.data == null){
+        ajaxGet(e.state.url);
+    } else {
+        ajaxPost(e.state.url, e.state.data)
+    }
+}
--- src/main/resources/static/js/blog.js
+++ src/main/resources/static/js/blog.js
@@ -3,35 +3,29 @@
 
 
 
-$("#search-btn").on("click",
-    function (e) {
-        e.preventDefault();
-        $("input[name='page']").val(0);
-        postData()
-        history.pushState(null, null, "/admin/blogs/search");
-    }
-)
 
-    let postData = function () {
-        $.ajax({
-            url: "/admin/blogs/search",
-            type: "POST",
-            data: {
-                title: $("[name='title']").val(),
-                typeId: $("[name='typeId']").val(),
-                recommend: $("[name='recommend']").prop('checked'),
-                page: $("[name='page']").val()
-            },
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader('Authorization', sessionStorage.getItem('token'));
-            },
-            success: function (data) {
-                $("#table-container").html(data)
-            }
-        })
-        history.pushState(null, null, "/admin/blogs/search");
-    }
 
+/* let postData = function () {
+    $.ajax({
+        url: "/admin/blogs/search",
+        type: "POST",
+        data: {
+            title: $("[name='title']").val(),
+            typeId: $("[name='typeId']").val(),
+            recommend: $("[name='recommend']").prop('checked'),
+            page: $("[name='page']").val()
+        },
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', sessionStorage.getItem('token'));
+        },
+        success: function (data) {
+            $("#table-container").html(data)
+        }
+    })
+    history.pushState(null, null, "/admin/blogs/search");
+} */
+
+/*
     const editBlog = function (blogId) {
         $.ajax({
             url: "/admin/blogs/" + blogId + "/update",
@@ -54,6 +48,7 @@ $("#search-btn").on("click",
         return true;
     }
 
+*/
 
 
 
@@ -61,14 +56,18 @@ $("#search-btn").on("click",
 
 
 /**
- *
+ * 坑1，
  * const 函数不能放在 document.read(function(){ }) 里头， 把js文件链接放在html里，函数不能被引用。
  *
+ * 坑2，
  * history.pushState() 里的第一个参数state有要求的，state必须为可以序列化的对象，对象的方法无法被序列化，
  * 如果传入history.pushState()的第一个参数对象，是通过Object.create()创建的对象，并且包含了无法被序列化的方法，
  * 使用history.pushState()方法不会报错，但会把参数对象里的方法自动排除，用JSON.stringify()序列化此对象
  * 会有一样效果，然而，如果对象时通过构造函数创建的，则传入到history.pushState()里会报错
  * 错误为: Failed to execute 'pushState' on 'History': function(){} could not be cloned.
  *
+ * 坑3，
+ * 原页面加载完ajax返回页面之后，原有click事件监听不到，必须用$(document).on("click", "#add-blog",f)来监听
+ * 原来是$("#add-blog").on("click", f)
  *
  * */
